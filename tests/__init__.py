2257ce0e4ca632e5a35c537fff5a7220d649b006
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-03-20 12:46:05 +0000

    Update __init__.py
    add login,logout to tests init

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,13 +16,7 @@ vbanrs = {kind.id: vbancmd.connect(_kind, **opts) for kind in kinds.all}
 tests = vbanrs[_kind]
 
 def setup_package():
-    tests._modes = Modes()
-    tests._rt_packet_socket.bind((socket.gethostbyname(socket.gethostname()), tests._port))
-    tests.worker = Thread(target=tests._send_register_rt, daemon=True)
-    tests.worker.start()
-    tests._public_packet = tests._get_rt()
-    tests.worker2 = Thread(target=tests._keepupdated, daemon=True)
-    tests.worker2.start()
+    tests.login()
 
 def teardown_package():
-    tests.close()
+    tests.logout()
