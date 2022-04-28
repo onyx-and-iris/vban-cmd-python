80ecd7daefa088a23b22eddbfc4443b8d4806e48
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-04-28 11:39:22 +0100

    generate potato tests badge

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,8 @@ import random
 import sys
 
 # let's keep things random
-kind_id = random.choice(("basic", "banana", "potato"))
+# kind_id = random.choice(("basic", "banana", "potato"))
+kind_id = "potato"
 
 opts = {
     "ip": "codey.local",
