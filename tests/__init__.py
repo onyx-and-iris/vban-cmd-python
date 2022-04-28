98c15967d14279f63a265a9efdeb9fbb4c7acc3f
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-04-28 11:40:14 +0100

    set tests to run randomly

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,8 +5,7 @@ import random
 import sys
 
 # let's keep things random
-# kind_id = random.choice(("basic", "banana", "potato"))
-kind_id = "potato"
+kind_id = random.choice(("basic", "banana", "potato"))
 
 opts = {
     "ip": "codey.local",
