e6ba5aefe93063b9244f77d6ace7d2f5f574cb55
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-04-29 23:29:14 +0100

    minor version bump, changes to profiles loading

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="vbancmd",
-    version="0.0.1",
+    version="0.0.2",
     description="VBAN CMD Python API",
     packages=["vbancmd"],
     install_requires=["toml"],
