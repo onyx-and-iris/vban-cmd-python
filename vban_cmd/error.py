5bb0c2731e6979da7a0fafa8b46abd317f636995
onyx-and-iris <code@onyxandiris.online>
2023-06-25 13:51:30 +0100

    run through black

--- vban_cmd/error.py
+++ vban_cmd/error.py
@@ -1,5 +1,6 @@
 class VBANCMDError(Exception):
     """Exception raised when general errors occur"""
 
+
 class VBANCMDConnectionError(Exception):
     """Exception raised when connection/timeout errors occur"""
