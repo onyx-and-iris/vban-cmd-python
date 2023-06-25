54041503c952292d82dec7eb7d24670ec21d9d3b
onyx-and-iris <code@onyxandiris.online>
2023-06-25 15:00:23 +0100

    add gui, tests to scripts
    add tox to development dependencies
    
    major version bump

--- scripts.py
+++ scripts.py
@@ -2,6 +2,11 @@ import subprocess
 from pathlib import Path
 
 
+def ex_gui():
+    path = Path.cwd() / "examples" / "gui" / "."
+    subprocess.run(["py", str(path)])
+
+
 def ex_obs():
     path = Path.cwd() / "examples" / "obs" / "."
     subprocess.run(["py", str(path)])
@@ -10,3 +15,7 @@ def ex_obs():
 def ex_observer():
     path = Path.cwd() / "examples" / "observer" / "."
     subprocess.run(["py", str(path)])
+
+
+def test():
+    subprocess.run(["tox"])
