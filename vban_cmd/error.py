bd6e57b3c61f929e2915ae3d8363ea9d9cc04068
onyx-and-iris <code@onyxandiris.online>
2023-08-07 16:31:08 +0100

    define message attribute for VBANCMD error classes
    override str magic method

--- vban_cmd/error.py
+++ vban_cmd/error.py
@@ -1,6 +1,13 @@
 class VBANCMDError(Exception):
     """Base VBANCMD Exception class. Raised when general errors occur"""
 
+    def __init__(self, msg):
+        self.message = msg
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{type(self).__name__}: {self.message}"
+
 
 class VBANCMDConnectionError(VBANCMDError):
     """Exception raised when connection/timeout errors occur"""
