27a26b8fe9d8d2afdc2c2d6083c8ba8e9fa9279e
onyx-and-iris <code@onyxandiris.online>
2023-08-13 18:15:31 +0100

    remove __str__ override

--- vban_cmd/error.py
+++ vban_cmd/error.py
@@ -1,13 +1,6 @@
 class VBANCMDError(Exception):
     """Base VBANCMD Exception class. Raised when general errors occur"""
 
-    def __init__(self, msg):
-        self.message = msg
-        super().__init__(self.message)
-
-    def __str__(self):
-        return f"{type(self).__name__}: {self.message}"
-
 
 class VBANCMDConnectionError(VBANCMDError):
     """Exception raised when connection/timeout errors occur"""
